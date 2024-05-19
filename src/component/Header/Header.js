081b105fe563f648c5d13ea5bd1c55135b4fba0c
Galib Mia <gm71bd@gmail.com>
2024-05-19 07:55:53 +0600

    Header section added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='header-title'>DevWisdom</h1>
+            <img className='header-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi7XGKrg50D2Uq9m3Y2RBAc-CfiFIkWOW6fP4LT3j7TA&s" alt="" />
+        </div>
+    );
+};
+
+export default Header;
